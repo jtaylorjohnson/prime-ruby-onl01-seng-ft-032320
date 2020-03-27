1c33d587ac413c69b96fcbdc68859940f483aae7
James Johnson <jtaylorjohnson@gmail.com>
2020-03-27 02:08:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(n)
   array = (2..(n-1))
   if n <= 1 
     return false
-  elsif n != 2 && array.each do |i| n % i == 0 
-    return false 
+  elsif n != 2 && array.any do |i| n % i == 0 
+     false 
     end
   else 
     return true 

577d0e291eea6870a50f3ef0d31ea90c52b19050
James Johnson <jtaylorjohnson@gmail.com>
2020-03-27 01:34:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(n)
   array = (2..n-1)
   if n <= 1 
     return false
-  elsif array.each do |i| n % i == 0 
+  elsif n != 3 && array.each do |i| n % i == 0 
     return false
     end
   else 

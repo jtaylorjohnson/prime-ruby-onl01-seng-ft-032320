04ea14fb34119885d9d40327e0d64c93eead1074
James Johnson <jtaylorjohnson@gmail.com>
2020-03-27 01:29:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+  array = (2..n)
+  if n <= 1 
+    return false
+  elsif array.each do |i| n % i == 0 
+    return false
+    end
+  else 
+    return true 
+  end
+end
